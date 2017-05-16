0fcc93a748b07c76019143ad422f216c05e50cdb
Corey Sader <corey.sader@gmail.com>
2017-05-16 12:05:54 -0500

    removed console.logs

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ if (majorKey >= 53){
 
 /*3. Add a string with a value of 11 to `majorKey` */
 majorKey += "11";
-console.log(majorKey);
+
 /*4. Create an array named `keyChars`, then loop through `majorKey` using the `charAt` method and set `keyChars[i]` to each value*/
 var keyChars= [];
 
@@ -23,28 +23,24 @@ var keyChars= [];
     keyChars.push(majorKey.charAt(i));
     //keyChars[i]=majorKey.charAt(i);
   }
-  console.log(majorKey);
+
 /*5. Remove the first and last values from `keyChars`*/
-console.log(keyChars);
 keyChars.shift();
 keyChars.pop();
-  console.log(majorKey);
-  console.log(keyChars);
+
 /*6. Create a new variable. Loop **backwards** through `keyChars` and store each value into the new variable, combining each of the values of `keyChars`*/
 var backwards= '';
 for(i=keyChars.length; i>0; i--){
   backwards +=keyChars[i-1];
 }
-console.log(backwards);
-console.log(majorKey);
+
 /*7. `parseInt` both the `majorKey` and the new variable created in Step 6, ensure that both `majorKey` and the new variable are set to these new parsed values.*/
 backwards=parseInt(backwards);
 majorKey=parseInt(majorKey);
-console.log(majorKey);
+
 /******** SWITCH PROGRAMMERS *******
 
 /*8. Add `majorKey` and the new variable created in Step 6 together and store them in `majorKey`*/
-console.log(backwards);
  majorKey += backwards;
 
 /*9. If the new value of `majorKey` is less than 60, set `majorKey` equal to 14. If not, check to see if it is equal to 2930, if it is, set `majorKey` equal to 27.
@@ -56,14 +52,14 @@ if(majorKey < 60){
 } else {
   majorKey = 2;
 }
-console.log(majorKey);
+
 /*10. Create a while loop that counts down from 10 and increments `majorKey` by 1.*/
 var i = 10;
 while(i > 0){
   majorKey++;
   i--;
 }
-console.log(majorKey);
+
 /*11. Create a function that accepts an argument `val`. The function should convert `val` to a string, then drop the first character off the string, but only if there
 is more than one character in the string. Return `val` and set `majorKey` to the returned value.*/
 function returnVal(val){
